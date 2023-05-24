1302c1b3c2329f57792f7216bac7f8f05ff5789c
Jm <jm.popo>
2023-05-24 19:59:37 +0800

    'error'

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,27 @@
-export default function Home() {
+import { useState,useEffect } from "react";
+import axios from 'axios';
+
+
+function Homes(){
+  const [output,setOutput] = useState([]);
+    useEffect(() =>{
+      axios.get('https://api.publicapis.org/entries')
+      .then(response => {
+        console.log(response.data.entries);
+        setOutput(response.data.entries);
+        setOutput('asdasd');
+    });
+
+
+function Home() {
+  
+  
     return (
+
       <div>
         <h1>Home</h1>
       </div>
-    )
+    );
 }
+
+export default Home;
